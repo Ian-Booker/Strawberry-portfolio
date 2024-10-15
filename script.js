e5e54c3f424b93e55f8f6018dfb479d0906de36a
Ian-Booker <ianb60654@gmail.com>
2024-10-15 12:58:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Dark Mode Toggle
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+
+// Responsive Navigation Toggle
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.
